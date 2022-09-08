8bd8936f6fe7ef98400e4f84a4f7141c60700326
Sebastián Guadalupe <sebastianguadalupe00@gmail.com>
2022-09-07 21:40:34 -0300

    product page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom"
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
+import Product from "./Pages/Product";
 
 function App() {
-  return <div className="App">
-    <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/categories/:id" element={<Home/>}/>
-      <Route path="/product/:id" element={<Home/>}/>
-      <Route path="/register" element={<Home/>}/>
-      <Route path="/about-our-project" element={<Home/>}/>
-      <Route path="/admin" element={<Home/>}/>
-      <Route path="/cart" element={<Home/>}/>
-      <Route path="/chekout" element={<Home/>}/>
-    </Routes>
-  </div>;
+	return (
+		<div className="App">
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/categories/:id" element={<Home />} />
+				<Route path="/product/:id" element={<Product />} />
+				<Route path="/register" element={<Home />} />
+				<Route path="/about-our-project" element={<Home />} />
+				<Route path="/admin" element={<Home />} />
+				<Route path="/cart" element={<Home />} />
+				<Route path="/chekout" element={<Home />} />
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
