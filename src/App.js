1a4522d0ac3391d8a85553cabd54f3e52086e258
JuanZubillaga <juanzubillagap@gmail.com>
2022-09-08 15:48:11 -0300

    Vista Category

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import Product from "./Pages/Product";
+import Category from "./Pages/Category";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/categories/:id" element={<Home />} />
+        <Route path="/category/:id" element={<Category />} />
         <Route path="/product/:id" element={<Product />} />
         <Route path="/register" element={<Home />} />
         <Route path="/about-our-project" element={<Home />} />
--- src/Pages/Category.jsx
+++ src/Pages/Category.jsx
@@ -2,22 +2,68 @@ import React from "react";
 import style from "./Product.module.css";
 import Navbar from "../components/Navbar/Navbar";
 import Footer from "../components/Footer/Footer";
-import { Row, Col, Image, Button } from "react-bootstrap";
+import { Row, Col, Image, Button, Container } from "react-bootstrap";
 
-export default function Home() {
+export default function Category() {
   return (
     <>
       <Navbar />
-      <Row>
-        <Col lg={6}></Col>
-        <Col lg={6}></Col>
-        <Col lg={6}></Col>
-        <Col lg={6}></Col>
-        <Col lg={6}></Col>
-        <Col lg={6}></Col>
-        <Col lg={6}></Col>
-        <Col lg={6}></Col>
-      </Row>
+      <Container>
+        <Row>
+          <Col
+            lg={6}
+            className={`${style.product} d-flex flex-column justify-content-center align-items-center align-items-lg-center p-0 order-1`}
+          >
+            <Image
+              fluid
+              className={style.imageProduct}
+              src="https://images.ligne-roset.com/cache/products/3401/3d-views/1/1/11370600_6577_vue1_1500x1500.jpg"
+            />
+            <h2 className="text-start fs-2">PRADO</h2>
+            <span className="fst-italic fw-semibold fs-8">USD 10,585</span>
+            <Button>ADD TO CART</Button>
+          </Col>
+          <Col
+            lg={6}
+            className={`${style.product} d-flex flex-column justify-content-center align-items-center align-items-lg-center p-0 order-1`}
+          >
+            <Image
+              fluid
+              className={style.imageProduct}
+              src="https://images.ligne-roset.com/cache/products/3401/3d-views/1/1/11370600_6577_vue1_1500x1500.jpg"
+            />
+            <h2 className="text-start fs-2">PRADO</h2>
+            <span className="fst-italic fw-semibold fs-8">USD 10,585</span>
+            <Button>ADD TO CART</Button>
+          </Col>
+          <Col
+            lg={6}
+            className={`${style.product} d-flex flex-column justify-content-center align-items-center align-items-lg-center p-0 order-1`}
+          >
+            <Image
+              fluid
+              className={style.imageProduct}
+              src="https://images.ligne-roset.com/cache/products/3401/3d-views/1/1/11370600_6577_vue1_1500x1500.jpg"
+            />
+            <h2 className="text-start fs-2">PRADO</h2>
+            <span className="fst-italic fw-semibold fs-8">USD 10,585</span>
+            <Button>ADD TO CART</Button>
+          </Col>
+          <Col
+            lg={6}
+            className={`${style.product} d-flex flex-column justify-content-center align-items-center align-items-lg-center p-0 order-1`}
+          >
+            <Image
+              fluid
+              className={style.imageProduct}
+              src="https://images.ligne-roset.com/cache/products/3401/3d-views/1/1/11370600_6577_vue1_1500x1500.jpg"
+            />
+            <h2 className="text-start fs-2">PRADO</h2>
+            <span className="fst-italic fw-semibold fs-8">USD 10,585</span>
+            <Button>ADD TO CART</Button>
+          </Col>
+        </Row>
+      </Container>
       <Footer />
     </>
   );
