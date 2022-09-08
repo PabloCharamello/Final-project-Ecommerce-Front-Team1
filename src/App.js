0999709d1f50f49bdf0698eb44a8dc5c2e7c7753
Pablo Charamello <pablocharamello@gmail.com>
2022-09-08 09:23:54 -0300

    pretierrc

--- src/App.js
+++ src/App.js
@@ -5,20 +5,20 @@ import Home from "./Pages/Home";
 import Product from "./Pages/Product";
 
 function App() {
-	return (
-		<div className="App">
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/categories/:id" element={<Home />} />
-				<Route path="/product/:id" element={<Product />} />
-				<Route path="/register" element={<Home />} />
-				<Route path="/about-our-project" element={<Home />} />
-				<Route path="/admin" element={<Home />} />
-				<Route path="/cart" element={<Home />} />
-				<Route path="/chekout" element={<Home />} />
-			</Routes>
-		</div>
-	);
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/categories/:id" element={<Home />} />
+        <Route path="/product/:id" element={<Product />} />
+        <Route path="/register" element={<Home />} />
+        <Route path="/about-our-project" element={<Home />} />
+        <Route path="/admin" element={<Home />} />
+        <Route path="/cart" element={<Home />} />
+        <Route path="/chekout" element={<Home />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
