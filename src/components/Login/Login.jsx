e7cc4ce92c4ac61fca18ab159e3efb9e5b9623ce
Sebastián Guadalupe <sebastianguadalupe00@gmail.com>
2022-09-12 09:33:35 -0300

    login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,8 +1,33 @@
+import axios from "axios";
+import { useState } from "react";
 import { OverlayTrigger, Popover, Form, Button } from "react-bootstrap";
 import { BiUser } from "react-icons/bi";
+import { useDispatch } from "react-redux";
+import { setUser } from "../../redux/user/userSlice";
 import style from "./Login.module.css";
 
 export default function Login() {
+  const [email, setEmail] = useState("seba@mail.com");
+  const [password, setPassword] = useState("password");
+  const [invalidCredentials, setInvalidCredentials] = useState("");
+  const dispatch = useDispatch();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const apiCall = await axios({
+        url: "users/token/",
+        method: "POST",
+        data: {
+          email,
+          password,
+        },
+      });
+      dispatch(setUser(apiCall.data));
+    } catch (error) {
+      setInvalidCredentials(error.response.data.error);
+    }
+  };
   return (
     <OverlayTrigger
       trigger="click"
@@ -11,17 +36,28 @@ export default function Login() {
       overlay={
         <Popover>
           <Popover.Body>
-            <Form.Floating>
-              <Form.Control type="email" />
-              <label htmlFor="floatingInput">Email address</label>
-            </Form.Floating>
-            <Form.Floating className="mt-3">
-              <Form.Control type="password" />
-              <label htmlFor="floatingPassword">Password</label>
-            </Form.Floating>
-            <Button variant="dark" className="mt-3 px-5">
-              Log in
-            </Button>
+            <Form onSubmit={handleLogin}>
+              <Form.Floating>
+                <Form.Control
+                  type="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <label htmlFor="floatingInput">Email address</label>
+              </Form.Floating>
+              <Form.Floating className="mt-3">
+                <Form.Control
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <label htmlFor="floatingPassword">Password</label>
+              </Form.Floating>
+              <Button type="submit" variant="dark" className="mt-3 px-5">
+                Log in
+              </Button>
+            </Form>
+            <p className="mt-3 fw-bold">{invalidCredentials}</p>
           </Popover.Body>
         </Popover>
       }
