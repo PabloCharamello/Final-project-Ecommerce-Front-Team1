7b99e20a6ba445d82d09632c24adfa73d3607151
Pablo Charamello <pablocharamello@gmail.com>
2022-09-21 13:24:12 -0300

    conditional rendering added in navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -26,51 +26,53 @@ function BasicExample() {
   useEffect(() => getCategoriesFromApi, []);
 
   return (
-    <>
-      <Navbar sticky="top" expand="lg" className={`${style.background} px-lg-5`}>
-        <Container fluid>
-          <Navbar.Brand as={Link} to="/" className={style.logoNavbar}>
-            Hack Design
-          </Navbar.Brand>
-          <img src="../../assets/img/logo.png" alt="" />
-          <Navbar.Toggle />
-          <Navbar.Collapse>
-            <Nav className="mx-auto my-2 my-lg-0 justify-content-center">
-              <Nav.Link as={Link} to="/" className="fw-bold text-black">
-                HOME
-              </Nav.Link>
-              <NavDropdown
-                title={<span className="fw-bold text-black my-auto">CATEGORIES</span>}
-                className="mx-5"
-              >
-                {categories &&
-                  categories.map((category) => {
-                    return (
-                      <NavDropdown.Item
-                        key={category.id}
-                        as={Link}
-                        to={"/category/" + category.slug}
-                      >
-                        {category.name}
-                      </NavDropdown.Item>
-                    );
-                  })}
-              </NavDropdown>
-              <Nav.Link as={Link} to="/about-our-project" className="fw-bold text-black">
-                OUR PROJECT
-              </Nav.Link>
-              <Link className={style.aboutProjectDiv} as={Link} to="/about-our-project">
-                ABOUT OUR PROJECT
-              </Link>
-            </Nav>
-            <div className="d-flex justify-content-center">
-              <CartPopup />
-              {user.token ? <UserNavbar /> : <Login />}
-            </div>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </>
+    categories && (
+      <>
+        <Navbar sticky="top" expand="lg" className={`${style.background} px-lg-5`}>
+          <Container fluid>
+            <Navbar.Brand as={Link} to="/" className={style.logoNavbar}>
+              Hack Design
+            </Navbar.Brand>
+            <img src="../../assets/img/logo.png" alt="" />
+            <Navbar.Toggle />
+            <Navbar.Collapse>
+              <Nav className="mx-auto my-2 my-lg-0 justify-content-center">
+                <Nav.Link as={Link} to="/" className="fw-bold text-black">
+                  HOME
+                </Nav.Link>
+                <NavDropdown
+                  title={<span className="fw-bold text-black my-auto">CATEGORIES</span>}
+                  className="mx-5"
+                >
+                  {categories &&
+                    categories.map((category) => {
+                      return (
+                        <NavDropdown.Item
+                          key={category.id}
+                          as={Link}
+                          to={"/category/" + category.slug}
+                        >
+                          {category.name}
+                        </NavDropdown.Item>
+                      );
+                    })}
+                </NavDropdown>
+                <Nav.Link as={Link} to="/about-our-project" className="fw-bold text-black">
+                  OUR PROJECT
+                </Nav.Link>
+                <Link className={style.aboutProjectDiv} as={Link} to="/about-our-project">
+                  ABOUT OUR PROJECT
+                </Link>
+              </Nav>
+              <div className="d-flex justify-content-center">
+                <CartPopup />
+                {user.token ? <UserNavbar /> : <Login />}
+              </div>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </>
+    )
   );
 }
 
