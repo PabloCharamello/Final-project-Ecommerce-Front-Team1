3b73bb1ba1e1abae6ced220b446da0f0f012d400
Sebastián Guadalupe <sebastianguadalupe00@gmail.com>
2022-09-22 14:42:02 -0300

    correct prices on carts

--- src/Pages/ShoppingCart.jsx
+++ src/Pages/ShoppingCart.jsx
@@ -13,9 +13,12 @@ import {
   removeProductFromCart,
 } from "../redux/cart/cartSlice";
 import swal from "sweetalert";
+import { useState, useEffect } from "react";
 
 export default function ShoppingCart() {
   const cart = useSelector((state) => state.cart);
+  const user = useSelector((state) => state.user);
+  const [totalPrice, setTotalPrice] = useState(0);
   const dispatch = useDispatch();
 
   const modalConfirmation = (productId) => {
@@ -60,85 +63,118 @@ export default function ShoppingCart() {
     currency: "USD",
     maximumFractionDigits: 0,
   });
-  return (
-    <div>
-      <Navbar />
-      <Container>
-        <Row className="mb-5">
-          <h1 className="fw-3 mb-3">SHOPPING CART</h1>
-          <ToastContainer />
-          <Col lg={8}>
-            {cart.productsList.map((product) => {
-              return (
-                <div className="d-flex align-items-center w-100">
-                  <IoClose
-                    role="button"
-                    className={`cursor-pointer me-3`}
-                    size="20px"
-                    style={{ width: "20px" }}
-                    onClick={() => {
-                      modalConfirmation(product.id);
-                    }}
-                  />
-                  <Row key={product.id} className="border-bottom w-100">
-                    <Col lg={3} className={`d-flex justify-content-around align-items-center py-3`}>
-                      <Image fluid className={style.productImage} src={product.images[0]} />
-                    </Col>
-                    <Col lg={3} className={`d-flex justify-content-around align-items-center py-3`}>
-                      <span className="fw-bold text-black">{product.name}</span>
-                      <span className="">{priceFormatter.format(parseInt(product.price))}</span>
-                    </Col>
-                    <Col lg={6} className={`d-flex justify-content-around align-items-center py-3`}>
-                      <div className="d-flex align-items-center justify-content-center border px-4 py-3 user-select-none">
-                        <span className="me-3">Quantity</span>
-                        <IoCaretBack
-                          role="button"
-                          className="fs-3 mx-2 opacity-50"
-                          onClick={() => {
-                            handleMinusFromCart(product.id);
-                          }}
-                        />
 
-                        <span className="fs-5">{product.quantity}</span>
-                        <IoCaretForward
-                          role="button"
-                          className="fs-3 mx-2 opacity-50"
-                          onClick={() => {
-                            handleAddToCart(product);
-                          }}
-                        />
-                      </div>
-                      <span className="">
-                        {priceFormatter.format(parseInt(product.price * product.quantity))}
-                      </span>
-                    </Col>
-                  </Row>
+  useEffect(() => {
+    let newTotal = 0;
+    console.log(cart);
+    for (const product of cart.productsList) {
+      newTotal += product.price * product.quantity;
+    }
+    setTotalPrice(newTotal);
+  }, [cart]);
+
+  return (
+    <div className={`${style.shoppingCart} d-flex flex-column justify-content-between`}>
+      <Navbar />
+      <Container className={style.shoppingContainer}>
+        <h1 className="fw-3 mb-3">SHOPPING CART</h1>
+        <Row className="mb-5">
+          <ToastContainer />
+          {cart.productsList.length === 0 ? (
+            <h3>Your cart is empty</h3>
+          ) : (
+            <>
+              <Col lg={8}>
+                {cart.productsList.map((product) => {
+                  return (
+                    <div className="d-flex align-items-center w-100">
+                      <IoClose
+                        role="button"
+                        className={`cursor-pointer me-3`}
+                        size="20px"
+                        style={{ width: "20px" }}
+                        onClick={() => {
+                          modalConfirmation(product.id);
+                        }}
+                      />
+                      <Row key={product.id} className="border-bottom w-100">
+                        <Col
+                          lg={3}
+                          className={`d-flex justify-content-around align-items-center py-3`}
+                        >
+                          <Image fluid className={style.productImage} src={product.images[0]} />
+                        </Col>
+                        <Col
+                          lg={3}
+                          className={`d-flex justify-content-around align-items-center py-3`}
+                        >
+                          <span className="fw-bold text-black">{product.name}</span>
+                          <span className="">{priceFormatter.format(parseInt(product.price))}</span>
+                        </Col>
+                        <Col
+                          lg={6}
+                          className={`d-flex justify-content-around align-items-center py-3`}
+                        >
+                          <div className="d-flex align-items-center justify-content-center border px-4 py-3 user-select-none">
+                            <span className="me-3">Quantity</span>
+                            <IoCaretBack
+                              role="button"
+                              className="fs-3 mx-2 opacity-50"
+                              onClick={() => {
+                                handleMinusFromCart(product.id);
+                              }}
+                            />
+
+                            <span className="fs-5">{product.quantity}</span>
+                            <IoCaretForward
+                              role="button"
+                              className="fs-3 mx-2 opacity-50"
+                              onClick={() => {
+                                handleAddToCart(product);
+                              }}
+                            />
+                          </div>
+                          <span className="">
+                            {priceFormatter.format(parseInt(product.price * product.quantity))}
+                          </span>
+                        </Col>
+                      </Row>
+                    </div>
+                  );
+                })}
+              </Col>
+              <Col lg={4}>
+                <div className={`${style.cartTotals} d-flex flex-column mt-3`}>
+                  <h2 className="mb-3">CART TOTALS</h2>
+                  <div>
+                    <span className="me-3 fw-bold">SUBTOTAL</span>
+                    <span className="">{priceFormatter.format(totalPrice)}</span>
+                  </div>
+                  <div className="border-bottom pb-4 bg-gray">
+                    <span className="me-3 fw-bold">SHIPPING</span>
+                    <span className="">Free</span>
+                  </div>
+                  <div className="pt-4">
+                    <span className="fw-bold me-5 fs-3">TOTAL</span>
+                    <span className="fs-3">{priceFormatter.format(totalPrice)}</span>
+                    <div className="d-flex justify-content-center">
+                      <Button
+                        as={Link}
+                        to="/shipping"
+                        variant="dark"
+                        className={`${style.checkoutButton} ${
+                          !user.token && style.checkoutButtonDisabled
+                        }`}
+                        disabled={user.token ? false : true}
+                      >
+                        PROCEED TO CHECKOUT
+                      </Button>
+                    </div>
+                  </div>
                 </div>
-              );
-            })}
-          </Col>
-          <Col lg={4}>
-            <div className={`${style.cartTotals} d-flex flex-column mt-3`}>
-              <h2 className="mb-3">CART TOTALS</h2>
-              <div>
-                <span className="me-3 fw-bold">SUBTOTAL</span>
-                <span className="">$31,755</span>
-              </div>
-              <div className="border-bottom pb-4 bg-gray">
-                <span className="me-3 fw-bold">SHIPPING</span>
-                <span className="">Free</span>
-              </div>
-              <div className="pt-4">
-                <span className="fw-bold me-5 fs-3">TOTAL</span>
-                <span className="fs-3">$31,755</span>
-                <div className="d-flex justify-content-center">
-                  <Button as={Link} to="/shipping" variant="dark" className={style.checkoutButton}>
-                    PROCEED TO CHECKOUT
-                  </Button>
-                </div>
-              </div>
-            </div>
-          </Col>
+              </Col>
+            </>
+          )}
         </Row>
       </Container>
       <Footer />
